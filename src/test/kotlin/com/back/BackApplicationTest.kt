066b205d417ca72ddf6b698c9eb3825a49aa80cb
jhs512 <jangka512@gmail.com>
2025-05-05 15:35:48 +0900

    llama 4 scout test

--- src/test/kotlin/com/back/BackApplicationTest.kt
+++ src/test/kotlin/com/back/BackApplicationTest.kt
@@ -1,13 +1,21 @@
 package com.back
 
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.springframework.ai.openai.OpenAiChatModel
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
 class BackApplicationTest {
 
+    @Autowired
+    lateinit var chatClient: OpenAiChatModel
+
     @Test
-    fun contextLoads() {
+    @DisplayName("test chat model")
+    fun t1() {
+        println(chatClient.call("너를 소개해줘"))
     }
 
 }
